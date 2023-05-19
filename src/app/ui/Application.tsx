b5a464433e8b025049251a9a208e0c9c75d79261
Tigran Avushian <54512704+TigranAvushyan@users.noreply.github.com>
2023-05-19 21:03:35 +0400

    feat: init effector

--- src/app/ui/Application.tsx
+++ src/app/ui/Application.tsx
@@ -1,27 +1,11 @@
 import { FC } from 'react';
 import { NavigationProvider } from '../providers/NavigationProvider';
-import MapView, { Marker } from 'react-native-maps';
-import { View } from 'react-native';
+import { Text } from 'react-native';
 
 export const Application: FC = () => {
   return (
     <NavigationProvider>
-      <View className='flex-1'>
-        <MapView
-          initialRegion={{
-            latitude: 40.353792,
-            longitude: 43.9681024,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-          className='w-full h-full'
-        >
-          <Marker
-            coordinate={{ latitude: 40.353792, longitude: 43.9681024 }}
-            title={"I'm here"}
-          />
-        </MapView>
-      </View>
+      <Text>hello</Text>
     </NavigationProvider>
   );
 };
